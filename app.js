09522ca7a2ed7ee680841446d4fd282468bf4ad6
gitsmin <sendex485@gmail.com>
2020-08-09 04:08:09 +0900

    2.10 MVC 2:routes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express, { Router } from "express";
+import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
@@ -6,6 +6,7 @@ import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
 
 const app = express();
 
@@ -15,8 +16,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,7 +1,8 @@
 import express from "express";
+import routes from "../routes";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", (req, res) => res.send("Upload a video"));
+videoRouter.get(routes.upload, (req, res) => res.send("Upload a video"));
 
 export default videoRouter;
