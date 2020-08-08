4d75ab1b254520eabf1e53f0904f15d90e0482ae
gitsmin <sendex485@gmail.com>
2020-08-08 14:17:07 +0900

    create init

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("Home"));
+
+export default app;
---
+++ init.js
@@ -0,0 +1,8 @@
+import app from "./app";
+
+const PORT = 4000;
+
+const handleListen = () =>
+  console.log(`✅ Listening on: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListen);
