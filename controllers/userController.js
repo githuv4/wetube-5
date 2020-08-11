cde2302bc5cf7766111b884241bc002a362e4374
gitsmin <sendex485@gmail.com>
2020-08-12 06:35:14 +0900

    2.11 MVC 3:controllers

---
+++ controllers/userController.js
@@ -0,0 +1,3 @@
+export const userDetail = (req, res) => res.send("User Detail");
+export const editProfile = (req, res) => res.send("Edit User");
+export const changePassword = (req, res) => res.send("Change Password");
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,15 @@
 import express from "express";
+import routes from "../routes";
+import {
+  userDetail,
+  editProfile,
+  changePassword,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get("/", (req, res) => res.send("User Index"));
-userRouter.get("/edit", (req, res) => res.send("User Edit"));
-userRouter.get("/password", (req, res) => res.send("User Password"));
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
