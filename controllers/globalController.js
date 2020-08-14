d01f745c3550e81cc3e70d04edf91546e1dc340d
githuv4 <sendex485@gmail.com>
2020-08-14 21:56:37 +0900

    2.18 search Controller

--- controllers/globalController.js
+++ controllers/globalController.js
@@ -1,3 +1,11 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
+
 export const login = (req, res) => res.render("login", { pageTitle: "Login" });
 export const join = (req, res) => res.render("join", { pageTitle: "Join" });
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from "../routes";
-import { home, login, join } from "../controllers/globalController";
+import { home, login, join, search } from "../controllers/globalController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.join, join);
+globalRouter.get(routes.search, search);
 
 export default globalRouter;
