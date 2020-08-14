ec2ba0d2d8615f9b0ec0847876becb25dd8e93bb
githuv4 <sendex485@gmail.com>
2020-08-14 09:35:02 +0900

    2.15 Partials with pug

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import routes from "../routes";
-import { home } from "../controllers/globalController";
+import { home, login, join } from "../controllers/globalController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.login);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.join, join);
 
 export default globalRouter;
